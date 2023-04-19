87a1b439f92aa32d64bf570a14c9bdbe3db52dd4
Amalendu Pandey <amalendupandey56@gmail.com>
2023-04-19 19:15:32 +0530

    env variables used

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,19 +1,19 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../.env.local") });
 
-export const PORT = process.env.PORT || 3003;
+export const PORT = process.env.PORT || "";
 
-export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
+export const MONGO_URI = process.env.MONGO_URI ||"";
 
-export const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "taskManagerMaster";
+export const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "";
 
 export const BCRYPT_CONFIG = {
   saltRounds: 10,
 };
 
 export const JWT_CONFIG = {
-  secret: process.env.JWT_SECRET || "secret",
+  secret: process.env.JWT_SECRET || "",
   expiresIn: "1d",
 };
